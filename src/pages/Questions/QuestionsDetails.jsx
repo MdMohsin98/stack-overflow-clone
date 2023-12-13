1a196c687b4fb2dd7461e8474e234cb2d8801cc2
Mohammad Mohsin <mohammad.mohsin98012@gmail.com>
2023-12-13 11:32:36 +0530

    Questionsbar related ui changed and updated

--- src/pages/Questions/QuestionsDetails.jsx
+++ src/pages/Questions/QuestionsDetails.jsx
@@ -4,6 +4,7 @@ import upvote from '../../assets/sort-up.svg'
 import downvote from '../../assets/sort-down.svg'
 import './Questions.css'
 import Avatar from '../../components/Avatar/Avatar'
+import DisplayAnswer from './DisplayAnswer'
 
 const QuestionsDetails = () => {
 
@@ -77,9 +78,9 @@ const QuestionsDetails = () => {
                                 <h1>{question.questionTitle}</h1>
                                 <div className='question-details-container-2'>
                                     <div className='question-votes'>
-                                        <img src={upvote} alt="" width='18' />
+                                        <img src={upvote} alt="" width='18' className='votes-icon' />
                                         <p>{question.upVotes - question.downVotes}</p>
-                                        <img src={downvote} alt="" width='18' />
+                                        <img src={downvote} alt="" width='18' className='votes-icon' />
                                     </div>
                                     <div style={{width:"100%"}}>
                                       <p className='question-body'>{question.questionBody}</p>
@@ -90,7 +91,7 @@ const QuestionsDetails = () => {
                                           ))
                                         }
                                       </div>
-                                      <div className="question-action-user">
+                                      <div className="question-actions-user">
                                         <div>
                                           <button type='button'>Share</button>
                                           <button type='button'>Delete</button>
@@ -98,12 +99,39 @@ const QuestionsDetails = () => {
                                         <div>
                                           <p>Asked {question.askedOn}</p>
                                           <Link to={`/User/${question.userId}`} className='user-link' style={{color:'#0086d8'}}>
-                                            <Avatar />
+                                            <Avatar backgroundColor='orange' px='8px' py='5px'>{question.userPosted.charAt(0).toUpperCase()}</Avatar>
+                                            <div>
+                                              {question.userPosted}
+                                            </div>
                                           </Link>
                                         </div>
                                       </div>
                                     </div>
                                 </div>
+                            </section>
+                            {
+                              question.noOfAnswers !== 0 && (
+                                <section>
+                                  <h3>{question.noOfAnswers} answers</h3>
+                                  <DisplayAnswer key={question._id} question={question}/>
+                                </section>
+                              )
+                            }
+                            <section className='post-ans-container'>
+                              <h3>Your Answer</h3>
+                              <form>
+                                <textarea name="" id="" cols="30" rows="10"></textarea><br/>
+                                <input type="submit" className='post-ans-btn' value='Post your answer' />
+                              </form>
+                              <p>
+                                Browse other Question tagged
+                                {
+                                  question.questionTags.map((tag) => (
+                                    <Link to='/Tags' key={tag} className='ans-tags'> {tag} </Link>
+                                  ))
+                                }  or 
+                                <Link to='/AskQuestion' style={{textDecoration:"none", color:"#009dff"}}> ask your own question</Link>
+                              </p>
                             </section>
 
                         </div>
